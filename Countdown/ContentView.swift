fc53e7ece00d1800f431f410857264db71d587a4
DanNguyen7 <74895@stpeters.sa.edu.au>
2026-05-26 21:18:37 +0930

    Initial Commit

---
+++ Countdown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Countdown
+//
+//  Created by Dan Nguyen on 26/5/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Countdown/CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownApp.swift
+//  Countdown
+//
+//  Created by Dan Nguyen on 26/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
